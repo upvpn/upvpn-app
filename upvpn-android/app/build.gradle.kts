b9f6c76ed2437e50846dac55f32a070db35c6753
Himanshu Neema <himanshu@upvpn.app>
2026-02-18 19:45:59 -0800

    retrofit and okhttp upgrades

--- upvpn-android/app/build.gradle.kts
+++ upvpn-android/app/build.gradle.kts
@@ -164,9 +164,9 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
 
     implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.18")
 
