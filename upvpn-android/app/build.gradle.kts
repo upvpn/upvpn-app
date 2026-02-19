e5b3ece80a3b43e2eecfd9449bcb7de2aad5e186
Himanshu Neema <himanshu@upvpn.app>
2026-02-18 21:25:05 -0800

    allow previews to work

--- upvpn-android/app/build.gradle.kts
+++ upvpn-android/app/build.gradle.kts
@@ -193,6 +193,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2026.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
