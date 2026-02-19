e071eb5cbe8df796ef23bc8234429f45286d3e55
Himanshu Neema <himanshu@upvpn.app>
2026-02-19 10:43:54 -0800

    fix edge to edge on android 15+

--- upvpn-android/app/src/main/kotlin/app/upvpn/upvpn/MainActivity.kt
+++ upvpn-android/app/src/main/kotlin/app/upvpn/upvpn/MainActivity.kt
@@ -1,11 +1,15 @@
 package app.upvpn.upvpn
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -30,7 +34,20 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            // android 15+ enforces edge-to-edge
+            enableEdgeToEdge()
+        }
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            // android 15+ enforces edge-to-edge
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(0, systemBars.top * 9 / 10, 0, 0)
+                insets
+            }
+        }
 
         // initialize connection manager
         val app = application as VPNApplication
