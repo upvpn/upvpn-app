7dcd1d638572123fe11e680f2c62cbc133163bfd
Himanshu Neema <himanshu@upvpn.app>
2026-02-20 19:11:34 -0800

    fix for tabview tabs to appear on bottom

--- upvpn-apple/UpVPN/iOS/Views/MainViewContent.swift
+++ upvpn-apple/UpVPN/iOS/Views/MainViewContent.swift
@@ -101,6 +101,64 @@ struct MainViewContentNew: View {
     }
 }
 
+@available(iOS 18, *)
+struct MainViewContent18: View {
+    @State private var selectedTab = 2
+
+    @State private var showInspector = false
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            
+            Tab("Locations", systemImage: "location", value: 1) {
+                NavigationStack {
+                    if UIDevice.current.userInterfaceIdiom == .pad {
+                        NavigationSplitView {
+                            LocationsView(showMapInToolbar: UIDevice.current.userInterfaceIdiom == .phone)
+                        } detail: {
+                            LocationsMapView(
+                                coordinateSpan: .large
+                            )
+                            // to remove transparent background bar on top
+                            .toolbarBackground(.hidden, for: .automatic)
+                        }
+                        // hack: otherwise searchbox shows up on toolbar
+                        .searchable(text: .constant(""), placement: .sidebar)
+                        .navigationTitle("Locations")
+                        // hack: otherwise an empty toolbar (and navigation title) with big height shows up.
+                        .toolbar(.hidden)
+                    } else {
+                        LocationsView(showMapInToolbar: UIDevice.current.userInterfaceIdiom == .phone)
+                            .navigationTitle("Locations")
+                    }
+                }
+                .environment(\.horizontalSizeClass, .regular)
+            }
+
+            Tab("Home", systemImage: "house", value: 2) {
+                ResponsiveHomeView(onStatsTap: { showInspector.toggle() })
+                    .modifier(InspectorModifier(showInspector: $showInspector))
+                    .environment(\.horizontalSizeClass, .regular)
+            }
+
+
+            Tab(value: 3, content:{ NavigationStack {
+                SettingsView()
+                    .navigationTitle("Account")
+            }
+            .environment(\.horizontalSizeClass, .regular)
+            }, label: {
+                Label { Text("Account") } icon: {
+                    Image(systemName:"person")
+                        .overlay(Image(systemName: "gearshape"))
+                }})
+
+        }
+        .tabViewStyle(.tabBarOnly)
+        .environment(\.horizontalSizeClass, .compact)
+    }
+}
+
 struct InspectorModifier: ViewModifier {
     @Binding var showInspector: Bool
 
@@ -121,7 +179,9 @@ struct InspectorModifier: ViewModifier {
 
 struct MainViewContent : View {
     var body: some View {
-        if #available(iOS 16, *) {
+        if #available(iOS 18, *) {
+            MainViewContent18()
+        } else if #available(iOS 16, *) {
             MainViewContentNew()
         } else {
             MainViewContentOld()
